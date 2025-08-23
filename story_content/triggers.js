f93a3066176d138e69d065814b4a70d08152491d
ApriWilder <april.wilder@gmail.com>
2025-08-23 12:38:49 -0700

    8/23/2025

--- story_content/triggers.js
+++ story_content/triggers.js
@@ -2,6 +2,69 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
+      case "6LjzG3AL15j":
+        Script1();
+        break;
+      case "5lZ028fO0rQ":
+        Script2();
+        break;
+      case "6lelRpbuBMO":
+        Script3();
+        break;
+      case "5lFqcVQSBoH":
+        Script4();
+        break;
+      case "5YdhudikweS":
+        Script5();
+        break;
+      case "5iGPxyGE8IM":
+        Script6();
+        break;
+      case "5wF5Fh9EVMa":
+        Script7();
+        break;
+      case "6BgTpz5pWIj":
+        Script8();
+        break;
+      case "61CwS12cSky":
+        Script9();
+        break;
+      case "5YQeAqI76Pr":
+        Script10();
+        break;
+      case "6X4Pa7TcP0I":
+        Script11();
+        break;
+      case "6LdDxbSVzl9":
+        Script12();
+        break;
+      case "5Z5ckIpKJtR":
+        Script13();
+        break;
+      case "5WJpLipVZHP":
+        Script14();
+        break;
+      case "6a5C1oyYSly":
+        Script15();
+        break;
+      case "6RsjdTfnbhp":
+        Script16();
+        break;
+      case "5dqsPbDFqcp":
+        Script17();
+        break;
+      case "5toiOZQzXCj":
+        Script18();
+        break;
+      case "6NritCdElSb":
+        Script19();
+        break;
+      case "5fHuCu9k9NI":
+        Script20();
+        break;
+      case "5xcGkfqh07Z":
+        Script21();
+        break;
   }
 }
 
@@ -9,7 +72,15 @@ window.InitExecuteScripts = function()
 {
 var player = GetPlayer();
 var object = player.object;
+var once = player.once;
 var addToTimeline = player.addToTimeline;
 var setVar = player.SetVar;
 var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
 };
